2265667c37731357bd36a84f2634cefa7422440e
lilac008 <106725993+lilac008@users.noreply.github.com>
2022-11-08 13:27:51 +0900

    s
    s

---
+++ 새 폴더/0729_/Assets/Assets/Scripts/VRCircle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   //
+
+public class VRCircle : MonoBehaviour
+{
+
+    public Image imgCircle;
+    public float totalTime = 2.0f;
+
+    bool gvrStatus;
+    float gvrTimer;
+
+    
+
+
+    void Update()
+    {
+
+        if (gvrStatus) 
+        {
+            gvrTimer += Time.deltaTime;
+            imgCircle.fillAmount = gvrTimer / totalTime;
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if (Physics.Raycast(ray, out hit, 10)) 
+        {
+            if (imgCircle.fillAmount == 1 && hit.transform.CompareTag("Teleport")) 
+            {
+                hit.transform.gameObject.GetComponent<Teleport>().TeleportPlayer();
+            }
+        }
+    }
+
+
+
+    public void GVROn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GVROff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgCircle.fillAmount = 0;
+    }
+
+
+}
+
